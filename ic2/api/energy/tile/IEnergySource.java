bd6252cc3cd6fcfa4d39e5a161acd744f7fe8c19
jenkins2git@immibis.com <immibis+modapi@gmail.com>
2014-04-02 03:17:50 -0400

    Build 33

--- ic2/api/energy/tile/IEnergySource.java
+++ ic2/api/energy/tile/IEnergySource.java
@@ -5,13 +5,20 @@ package ic2.api.energy.tile;
  */
 public interface IEnergySource extends IEnergyEmitter {
 	/**
-	 * Energy output provided by the source.
+	 * Energy output provided by the source this tick.
 	 * This is typically Math.min(stored energy, max output/tick).
 	 * 
 	 * @return Energy offered this tick
 	 */
 	double getOfferedEnergy();
 
+	/**
+	 * Draw energy from this source's buffer.
+	 * 
+	 * If the source doesn't have a buffer, this is a no-op.
+	 * 
+	 * @param amount amount of EU to draw
+	 */
 	void drawEnergy(double amount);
 }
 
