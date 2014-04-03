b0953d65369a037f7e246ec1bfb9f59ab2b6a999
jenkins2git@immibis.com <immibis+modapi@gmail.com>
2014-04-03 12:14:27 -0400

    Build 34

--- ic2/api/energy/EnergyNet.java
+++ ic2/api/energy/EnergyNet.java
@@ -6,6 +6,8 @@ package ic2.api.energy;
  * 
  * The old EnergyNet methods missing in IEnergyNet have been migrated to events (load, unload) or
  * removed (tiles no longer emit energy actively, the energy net implementation requests it).
+ * 
+ * See ic2/api/energy/usage.txt for an overall description of the energy net api.
  */
 public final class EnergyNet {
 	/**
